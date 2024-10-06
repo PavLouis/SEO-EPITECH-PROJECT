31c8da98f4e686ac93217d25a439369470cee1fe
PavLouis <louis.pavlovsky@epitech.eu>
2024-10-06 19:27:38 +0200

    [ADD]: next-sitemap and title+ description

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://trolls-mint-essentials.com/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
