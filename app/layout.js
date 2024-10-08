3beeb8ffb7510fffa20cbaf79da6930c822d0ba3
PavLouis <louis.pavlovsky@epitech.eu>
2024-10-08 16:29:35 +0200

    [ADD]: products page and fix navivation

--- app/layout.js
+++ app/layout.js
@@ -1,33 +1,25 @@
-import localFont from "next/font/local";
-import "./globals.css";
-import HomePageCards from './HomePage/HomePageCards';
-import HomeVideo from "./HomePage/HomeVideo";
-import Footer from "./Footer";
-import { CLIENT_STATIC_FILES_RUNTIME_POLYFILLS_SYMBOL } from "next/dist/shared/lib/constants";
-import AboutUs from "./HomePage/AboutUs";
+import NavBar from "./HomePage/NavBar";
+import './globals.css'
 
 export const metadata = {
   title: "Troll's Mint Essentials - Unlock the Magic of Andarduft",
   description: "Discover the invigorating power of Andarduft, a legendary Icelandic plant, in our sustainable and luxurious mint teas, essential oils, body care, and edibles. Elevate your well-being with a touch of Nordic magic. Shop now for the best candy online, buy candy, and enjoy our candy sale! Visit our factory and see the manufacturing process. Read reviews and learn about the ingredients in our Andarduft candy.",
 };
 
-function Home() {
-  return (
-    <div>
-      <HomeVideo/>
-      <HomePageCards/>
-      <AboutUs/>
-      <Footer/>
-    </div>
-  );
-}
-
 export default function RootLayout({ children }) {
+
   return (
     <html>
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
       <body>
-        <Home/>
-        </body>
+        <NavBar />
+        <div>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        timesNewLightItalic: ["TimesNewLightItalic", "sans-serif"],
+      },
     },
   },
   plugins: [],
